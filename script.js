5b1d9a2e007a2cebafc38b6d5f0bde358499c61d
weiowyw <haxjlebatop@gmail.com>
2022-10-21 14:46:24 +0500

    ff

---
+++ script.js
@@ -0,0 +1,145 @@
+
+
+// let a = 10;
+// a = parseInt(a);
+//
+// for(let i = 0;i < 11;i++){
+//     document.write(a);
+//     if(!(i===10)){
+//         document.write(', ')
+//     }
+//     a++;
+//
+// }
+
+
+// let a = 10;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// for(let i = 0;i < 11;i++){
+//     b = 0;
+//     b = Math.pow(a,2);
+//     document.write(b);
+//     if(!(i===10)){
+//         document.write(', ')
+//     }
+//     a++;
+//
+// }
+
+// let a = 7;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// for(let i = 1; i <= 10; i++){
+//     b = a * i;
+//     document.write(a + ' * ' + i + ' = ' + b + ' ');
+// }
+
+// let a = 1;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// do{
+//     b = b + a;
+//     a++;
+// }while(a <= 15)
+// document.write(b);
+
+
+// let a = 15;
+// a = parseInt(a);
+// let b = 1;
+// b = parseInt(b);
+//
+// do{
+//     b = b * a;
+//     a++;
+// }while(a <= 35)
+// document.write(b);
+
+
+// let a = 1;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// do{
+//     b = b + a;
+//     a++;
+// }while(a <= 500)
+// b = b / 500;
+// document.write(b);
+
+
+// let a = 30;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// do{
+//     if(a % 2 === 0) {
+//         b = b + a;
+//     }
+//     a++;
+// }while(a <= 80)
+// document.write(b);
+
+
+// let a = 100;
+// a = parseInt(a);
+// let b = 0;
+// b = parseInt(b);
+//
+// do{
+//     if(a % 3 === 0) {
+//         b = b + a;
+//     }
+//     a++;
+// }while(a <= 200)
+// document.write(b);
+
+// let a;
+// let quantityEven = 0;
+// quantityEven = parseInt(quantityEven);
+// let quantity = 0;
+// quantity = parseInt(quantity);
+//
+// do {
+//     a = parseInt(prompt('Enter an integer'));
+// }while(a === 0)
+//
+// for(let i = 1; i<=10; i++){
+//     if(a % i === 0){
+//         document.write(i + ' ');
+//         quantity++;
+//         if(i % 2 === 0){
+//             quantityEven++;
+//         }
+//     }
+//
+// }
+// if(a % 2 === 0){
+//     quantityEven++;
+// }
+// document.write(a);
+// quantity++;
+// document.write('. The number of dividers is ' + quantity);
+// document.write('. The number of even dividers is ' + quantityEven);
+
+
+
+// let product = 0;
+// product = parseInt(product);
+//
+// for(let i = 1; i < 10; i++){
+//     for(let j = 1; j < 10; j++){
+//         product = i * j;
+//         document.write(i + ' * ' + j + ' = ' + product + ' ');
+//     }
+//     document.write('<br>');
+// }
